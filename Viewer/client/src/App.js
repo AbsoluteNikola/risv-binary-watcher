4ee67d246c30b9c2f30b6e0c07a0c6a94b6bcf83
kechin.vv <kechinv1@rambler.ru>
2023-11-19 20:58:47 +0300

    auto layout migrate to js

---
+++ Viewer/client/src/App.js
@@ -0,0 +1,161 @@
+import {initialNodes, initialEdges} from './elements.js';
+import ELK from 'elkjs';
+import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
+import ReactFlow, {
+    ReactFlowProvider,
+    addEdge,
+    Panel,
+    useNodesState,
+    useEdgesState,
+    useReactFlow,
+} from 'reactflow';
+import './App.css';
+import 'reactflow/dist/style.css';
+
+const elk = new ELK();
+
+
+const elkOptions = {
+    'elk.algorithm': 'layered',
+    'elk.layered.spacing.nodeNodeBetweenLayers': '100',
+    'elk.spacing.nodeNode': '80',
+};
+
+const getLayoutedElements = (nodes, edges, options = {}) => {
+    const isHorizontal = options?.['elk.direction'] === 'RIGHT';
+    const graph = {
+        id: 'root',
+        layoutOptions: options,
+        children: nodes.map((node) => ({
+            ...node,
+            // Adjust the target and source handle positions based on the layout
+            // direction.
+            targetPosition: isHorizontal ? 'left' : 'top',
+            sourcePosition: isHorizontal ? 'right' : 'bottom',
+
+            // Hardcode a width and height for elk to use when layouting.
+            width: 150,
+            height: 50,
+        })),
+        edges: edges,
+    };
+
+    return elk
+        .layout(graph)
+        .then((layoutedGraph) => ({
+            nodes: layoutedGraph.children.map((node) => ({
+                ...node,
+                // React Flow expects a position property on the node instead of `x`
+                // and `y` fields.
+                position: {x: node.x, y: node.y},
+            })),
+
+            edges: layoutedGraph.edges,
+        }))
+        .catch(console.error);
+};
+
+function App() {
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const {fitView} = useReactFlow();
+
+    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+    const onLayout = useCallback(
+        ({direction, useInitialNodes = false}) => {
+            const opts = {'elk.direction': direction, ...elkOptions};
+            const ns = useInitialNodes ? initialNodes : nodes;
+            const es = useInitialNodes ? initialEdges : edges;
+
+            getLayoutedElements(ns, es, opts).then(({nodes: layoutedNodes, edges: layoutedEdges}) => {
+                setNodes(layoutedNodes);
+                setEdges(layoutedEdges);
+
+                window.requestAnimationFrame(() => fitView());
+            });
+        },
+        [nodes, edges]
+    );
+
+    // Calculate the initial layout on mount.
+    useLayoutEffect(() => {
+        onLayout({direction: 'DOWN', useInitialNodes: true});
+    }, []);
+
+    // const [state, setState] = useState(null);
+
+    // const callBackendAPI = async () => {
+    //     const response = await fetch('/');
+    //     const body = await response.json();
+    //
+    //     if (response.status !== 200) {
+    //         throw Error(body.message)
+    //     }
+    //     return body;
+    // };
+    //
+    // useEffect(() => {
+    //     callBackendAPI()
+    //         .then(res => setState(res.express))
+    //         .catch(err => console.log(err));
+    // }, [])
+
+
+    // const onConnect = useCallback(
+    //     (params) => setEdges((eds) => addEdge(params, eds)),
+    //     [setEdges],
+    // );
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const onChangeFile = async (e) => {
+        const files = (e.target).files;
+
+        if (files != null) {
+            let file = files[0];
+            if (file == null) return;
+            console.log(file.text());
+            //let jsonData = JSON.parse(await file.text())
+            setNodes((nds) => nds.concat({id: '3333', position: {x: 0, y: 0}, data: {label: '1000'}}));
+            const forceLayout = document.getElementById("testLayoutClick");
+            await sleep(10);
+            forceLayout.click()
+        }
+    };
+
+
+    return (
+        <div style={{width: '100vw', height: '100vh'}}>
+            <div>
+                <input type="file" onChange={(e) => onChangeFile(e)}/>
+            </div>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onConnect={onConnect}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                fitView
+            >
+                <Panel position="top-right">
+                    <button id="testLayoutClick" onClick={() => onLayout({direction: 'DOWN'})}>vertical layout</button>
+
+                    <button onClick={() => onLayout({direction: 'RIGHT'})}>horizontal layout</button>
+                </Panel>
+            </ReactFlow>
+        </div>
+    );
+}
+
+export default () => (
+    <ReactFlowProvider>
+        <App/>
+    </ReactFlowProvider>
+);
+
+
+
+
+
+
