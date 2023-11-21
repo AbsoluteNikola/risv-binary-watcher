a74ec7972a4e90b65d769ec4c30bd205a21a739e
kechin.vv <kechinv1@rambler.ru>
2023-11-21 22:49:53 +0300

    select input and output

--- Viewer/client/src/App.js
+++ Viewer/client/src/App.js
@@ -8,10 +8,14 @@ import ReactFlow, {
     useNodesState,
     useEdgesState,
     useReactFlow,
+    MarkerType,
+    getOutgoers,
+    getIncomers,
+    isEdge,
+    isNode
 } from 'reactflow';
 import './App.css';
 import 'reactflow/dist/style.css';
-import {MarkerType} from "reactflow";
 
 const elk = new ELK();
 
@@ -64,6 +68,8 @@ function App() {
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
     const {fitView} = useReactFlow();
+
+    const [selectedNode, setSelectedNode] = useState(null);
 
     const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
     const onLayout = useCallback(
@@ -157,6 +163,183 @@ function App() {
         }
     };
 
+    const getAllIncomers = (node, nodes, edges, prevIncomers = []) => {
+        const incomers = getIncomers(node, nodes, edges);
+        const result = incomers.reduce(
+            (memo, incomer) => {
+                memo.push(incomer);
+
+                if ((prevIncomers.findIndex(n => n.id === incomer.id) === -1)) {
+                    prevIncomers.push(incomer);
+
+                    getAllIncomers(incomer, nodes, edges, prevIncomers).forEach((foundNode) => {
+                        memo.push(foundNode);
+
+                        if ((prevIncomers.findIndex(n => n.id === foundNode.id) === -1)) {
+                            prevIncomers.push(incomer);
+
+                        }
+                    });
+                }
+                return memo;
+            },
+            []
+        );
+        return result;
+    }
+
+    const getAllOutgoers = (node, nodes, edges, prevOutgoers = []) => {
+        const outgoers = getOutgoers(node, nodes, edges);
+        return outgoers.reduce(
+            (memo, outgoer) => {
+                memo.push(outgoer);
+
+                if ((prevOutgoers.findIndex(n => n.id === outgoer.id) === -1)) {
+                    prevOutgoers.push(outgoer);
+
+                    getAllOutgoers(outgoer, nodes, edges, prevOutgoers).forEach((foundNode) => {
+                        memo.push(foundNode);
+
+                        if ((prevOutgoers.findIndex(n => n.id === foundNode.id) === -1)) {
+                            prevOutgoers.push(foundNode);
+                        }
+                    });
+                }
+                return memo;
+            },
+            []
+        )
+    }
+
+    // const getIncomers = (node, nodes, edges) => {
+    //     if (!isNode(node)) {
+    //         return [];
+    //     }
+    //
+    //     const incomersIds = edges
+    //         .filter((e) => e.target === node.id)
+    //         .map((e) => e.source);
+    //
+    //     return nodes.filter((e) => incomersIds.map((id) => {
+    //         const matches = /([\w-^]+)__([\w-]+)/.exec(id);
+    //         if (matches === null) {
+    //             return id;
+    //         }
+    //         return matches[1];
+    //     }).includes(e.id));
+    // };
+    //
+    // const getOutgoers = (node, nodes, edges) => {
+    //     if (!isNode(node)) {
+    //         return [];
+    //     }
+    //
+    //     const outgoerIds = edges
+    //         .filter((e) => e.source === node.id)
+    //         .map((e) => e.target);
+    //
+    //     return nodes.filter((n) => outgoerIds.map((id) => {
+    //         const matches = /([\w-^]+)__([\w-]+)/.exec(id);
+    //         if (matches === null) {
+    //             return id;
+    //         }
+    //         return matches[1];
+    //     }).includes(n.id));
+    // };
+
+    function setElements(node, nodes, edges, selection) {
+        if (node && [...nodes, ...edges]) {
+            const allIncomers = getIncomers(node, nodes, edges);
+            const allOutgoers = getOutgoers(node, nodes, edges);
+
+            setNodes((prevElements) => {
+                return prevElements?.map((elem) => {
+                    const incomerIds = allIncomers.map((i) => i.id)
+                    const outgoerIds = allOutgoers.map((o) => o.id)
+
+                    if (isNode(elem) && (allOutgoers.length > 0 || allIncomers.length > 0)) {
+                        const highlight = elem.id === node.id || incomerIds.includes(elem.id) || outgoerIds.includes(elem.id)
+
+                        elem.style = {
+                            ...elem.style,
+                            opacity: highlight ? 1 : 0.25,
+                        }
+                    }
+                    return elem
+                })
+            });
+
+            setEdges((prevElements) => {
+                return prevElements?.map((elem) => {
+                    const incomerIds = allIncomers.map((i) => i.id)
+                    const outgoerIds = allOutgoers.map((o) => o.id)
+                    if (isEdge(elem)) {
+                        if (selection) {
+                            const animated_output =
+                                outgoerIds.includes(elem.target) && node.id === elem.source
+                            const animated_inp =
+                                incomerIds.includes(elem.source) && node.id === elem.target
+
+                            const animated = animated_inp || animated_output
+                            elem.animated = animated
+                            let color = '#FF0000'
+                            if (animated_inp) color = '#008000'
+                            elem.style = {
+                                ...elem.style,
+                                stroke: animated ? color : '#b1b1b7',
+                                strokeWidth: animated ? 2: 1,
+                                opacity: animated ? 1 : 0.25,
+                            }
+                        } else {
+                            elem.animated = false
+                            elem.style = {
+                                ...elem.style,
+                                stroke: '#b1b1b7',
+                                opacity: 1,
+                                strokeWidth: 1,
+                            }
+                        }
+                    }
+
+                    return elem
+                })
+            });
+        }
+    }
+
+    const highlightPath = (node, nodes, edges, selection) => {
+        setElements(node, nodes, edges, selection);
+    }
+
+    const resetNodeStyles = () => {
+        setNodes((prevElements) => {
+            return prevElements?.map((elem) => {
+                if (isNode(elem)) {
+                    elem.style = {
+                        ...elem.style,
+                        opacity: 1,
+                    }
+                }
+                return elem
+            })
+        });
+        setEdges((prevElements) => {
+            return prevElements?.map((elem) => {
+                if (isEdge((elem))) {
+                    elem.animated = false
+                    elem.style = {
+                        ...elem.style,
+                        stroke: '#b1b1b7',
+                        strokeWidth: 1,
+                        opacity: 1,
+                    }
+                }
+
+                return elem
+            })
+        })
+    }
+
 
     return (
         <div style={{width: '100vw', height: '100vh'}}>
@@ -166,10 +349,23 @@ function App() {
             <ReactFlow
                 nodes={nodes}
                 edges={edges}
-                onConnect={onConnect}
+                //onConnect={onConnect}
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 fitView
+                elementsSelectable={true}
+                // onNodeMouseEnter={(_event, node) => !selectedNode && highlightPath(node, [...nodes, ...edges])}
+                // onNodeMouseLeave={() => !selectedNode && resetNodeStyles()}
+                onSelectionChange={(selectedElements) => {
+                    const node = selectedElements.nodes[0]
+                    if (node == null) {
+                        resetNodeStyles()
+                        setSelectedNode(undefined)
+                    } else {
+                        setSelectedNode(node)
+                        highlightPath(node, nodes, edges, true)
+                    }
+                }}
             >
                 <Panel position="top-right">
                     <button id="testLayoutClick" onClick={() => onLayout({direction: 'DOWN'})}>vertical layout</button>
