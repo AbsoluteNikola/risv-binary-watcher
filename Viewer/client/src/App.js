b59ae797dc56e0d7b9354558a5a5ffae5379b891
kechin.vv <kechinv1@rambler.ru>
2023-11-21 22:59:52 +0300

    move highlight functions to highlight.js

--- Viewer/client/src/App.js
+++ Viewer/client/src/App.js
@@ -16,6 +16,7 @@ import ReactFlow, {
 } from 'reactflow';
 import './App.css';
 import 'reactflow/dist/style.css';
+import {highlightPath, resetNodeStyles} from "./highlight";
 
 const elk = new ELK();
 
@@ -163,176 +164,6 @@ function App() {
         }
     };
 
-    const getAllIncomers = (node, nodes, edges, prevIncomers = []) => {
-        const incomers = getIncomers(node, nodes, edges);
-        const result = incomers.reduce(
-            (memo, incomer) => {
-                memo.push(incomer);
-
-                if ((prevIncomers.findIndex(n => n.id === incomer.id) === -1)) {
-                    prevIncomers.push(incomer);
-
-                    getAllIncomers(incomer, nodes, edges, prevIncomers).forEach((foundNode) => {
-                        memo.push(foundNode);
-
-                        if ((prevIncomers.findIndex(n => n.id === foundNode.id) === -1)) {
-                            prevIncomers.push(incomer);
-
-                        }
-                    });
-                }
-                return memo;
-            },
-            []
-        );
-        return result;
-    }
-
-    const getAllOutgoers = (node, nodes, edges, prevOutgoers = []) => {
-        const outgoers = getOutgoers(node, nodes, edges);
-        return outgoers.reduce(
-            (memo, outgoer) => {
-                memo.push(outgoer);
-
-                if ((prevOutgoers.findIndex(n => n.id === outgoer.id) === -1)) {
-                    prevOutgoers.push(outgoer);
-
-                    getAllOutgoers(outgoer, nodes, edges, prevOutgoers).forEach((foundNode) => {
-                        memo.push(foundNode);
-
-                        if ((prevOutgoers.findIndex(n => n.id === foundNode.id) === -1)) {
-                            prevOutgoers.push(foundNode);
-                        }
-                    });
-                }
-                return memo;
-            },
-            []
-        )
-    }
-
-    // const getIncomers = (node, nodes, edges) => {
-    //     if (!isNode(node)) {
-    //         return [];
-    //     }
-    //
-    //     const incomersIds = edges
-    //         .filter((e) => e.target === node.id)
-    //         .map((e) => e.source);
-    //
-    //     return nodes.filter((e) => incomersIds.map((id) => {
-    //         const matches = /([\w-^]+)__([\w-]+)/.exec(id);
-    //         if (matches === null) {
-    //             return id;
-    //         }
-    //         return matches[1];
-    //     }).includes(e.id));
-    // };
-    //
-    // const getOutgoers = (node, nodes, edges) => {
-    //     if (!isNode(node)) {
-    //         return [];
-    //     }
-    //
-    //     const outgoerIds = edges
-    //         .filter((e) => e.source === node.id)
-    //         .map((e) => e.target);
-    //
-    //     return nodes.filter((n) => outgoerIds.map((id) => {
-    //         const matches = /([\w-^]+)__([\w-]+)/.exec(id);
-    //         if (matches === null) {
-    //             return id;
-    //         }
-    //         return matches[1];
-    //     }).includes(n.id));
-    // };
-
-    function setElements(node, nodes, edges, selection) {
-        if (node && [...nodes, ...edges]) {
-            const allIncomers = getIncomers(node, nodes, edges);
-            const allOutgoers = getOutgoers(node, nodes, edges);
-
-            setNodes((prevElements) => {
-                return prevElements?.map((elem) => {
-                    const incomerIds = allIncomers.map((i) => i.id)
-                    const outgoerIds = allOutgoers.map((o) => o.id)
-
-                    if (allOutgoers.length > 0 || allIncomers.length > 0) {
-                        const highlight = elem.id === node.id || incomerIds.includes(elem.id) || outgoerIds.includes(elem.id)
-
-                        elem.style = {
-                            ...elem.style,
-                            opacity: highlight ? 1 : 0.25,
-                        }
-                    }
-                    return elem
-                })
-            });
-
-            setEdges((prevElements) => {
-                return prevElements?.map((elem) => {
-                    const incomerIds = allIncomers.map((i) => i.id)
-                    const outgoerIds = allOutgoers.map((o) => o.id)
-                    if (selection) {
-                        const animated_output =
-                            outgoerIds.includes(elem.target) && node.id === elem.source
-                        const animated_inp =
-                            incomerIds.includes(elem.source) && node.id === elem.target
-
-                        const animated = animated_inp || animated_output
-                        elem.animated = animated
-                        let color = '#FF0000'
-                        if (animated_inp) color = '#008000'
-                        elem.style = {
-                            ...elem.style,
-                            stroke: animated ? color : '#b1b1b7',
-                            strokeWidth: animated ? 2 : 1,
-                            opacity: animated ? 1 : 0.25,
-                        }
-                    } else {
-                        elem.animated = false
-                        elem.style = {
-                            ...elem.style,
-                            stroke: '#b1b1b7',
-                            opacity: 1,
-                            strokeWidth: 1,
-                        }
-                    }
-
-
-                    return elem
-                })
-            });
-        }
-    }
-
-    const highlightPath = (node, nodes, edges, selection) => {
-        setElements(node, nodes, edges, selection);
-    }
-
-    const resetNodeStyles = () => {
-        setNodes((prevElements) => {
-            return prevElements?.map((elem) => {
-                elem.style = {
-                    ...elem.style,
-                    opacity: 1,
-                }
-                return elem
-            })
-        });
-        setEdges((prevElements) => {
-            return prevElements?.map((elem) => {
-                elem.animated = false
-                elem.style = {
-                    ...elem.style,
-                    stroke: '#b1b1b7',
-                    strokeWidth: 1,
-                    opacity: 1,
-                }
-                return elem
-            })
-        })
-    }
 
 
     return (
@@ -353,11 +184,11 @@ function App() {
                 onSelectionChange={(selectedElements) => {
                     const node = selectedElements.nodes[0]
                     if (node == null) {
-                        resetNodeStyles()
+                        resetNodeStyles(setNodes, setEdges)
                         setSelectedNode(undefined)
                     } else {
                         setSelectedNode(node)
-                        highlightPath(node, nodes, edges, true)
+                        highlightPath(node, nodes, edges, true, setNodes, setEdges)
                     }
                 }}
             >
